606787946538315ef46d78b64a451c1b0a70a5fc
w00t <w00t@d29a8553-f4ab-da11-ae27-005070d7a3fb>
2006-03-10 05:20:25 +0000

    Added mode list dialog.
    git-svn-id: file://localhost/var/svnroot/obsidian/trunk@55 d29a8553-f4ab-da11-ae27-005070d7a3fb

---
+++ mcModeList.cs
@@ -0,0 +1,48 @@
+/*
+ * User: Rob
+ * Date: 10/03/2006
+ * Time: 2:52 PM
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Obsidian
+{
+	/// <summary>
+	/// Description of mcModeList.
+	/// </summary>
+	public class mcModeList : System.Windows.Forms.Form
+	{
+		public mcModeList()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		#region Windows Forms Designer generated code
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// mcModeList
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 266);
+			this.Text = "mcModeList";
+			this.Name = "mcModeList";
+		}
+		#endregion
+	}
+}
