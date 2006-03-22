798746b23bee59c63c1d9394eeee438a07576239
w00t <w00t@d29a8553-f4ab-da11-ae27-005070d7a3fb>
2006-03-22 20:16:37 +0000

    Mode dialog fiddling.. still not anywhere far.
    git-svn-id: file://localhost/var/svnroot/obsidian/trunk@77 d29a8553-f4ab-da11-ae27-005070d7a3fb

--- mcModeList.cs
+++ mcModeList.cs
@@ -37,21 +37,21 @@ namespace Obsidian
 		{
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.chkSingleModes = new System.Windows.Forms.CheckedListBox();
+			this.tabPage6 = new System.Windows.Forms.TabPage();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.tabPage3 = new System.Windows.Forms.TabPage();
 			this.tabPage4 = new System.Windows.Forms.TabPage();
 			this.tabPage5 = new System.Windows.Forms.TabPage();
-			this.tabPage6 = new System.Windows.Forms.TabPage();
-			this.label1 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.listBox1 = new System.Windows.Forms.ListBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.chkSingleModes = new System.Windows.Forms.CheckedListBox();
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
-			this.tabPage2.SuspendLayout();
 			this.tabPage6.SuspendLayout();
+			this.tabPage2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl1
@@ -79,6 +79,43 @@ namespace Obsidian
 			this.tabPage1.Text = "Single Modes";
 			this.tabPage1.UseVisualStyleBackColor = true;
 			// 
+			// chkSingleModes
+			// 
+			this.chkSingleModes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+									| System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.chkSingleModes.FormattingEnabled = true;
+			this.chkSingleModes.Location = new System.Drawing.Point(6, 6);
+			this.chkSingleModes.Name = "chkSingleModes";
+			this.chkSingleModes.Size = new System.Drawing.Size(544, 244);
+			this.chkSingleModes.TabIndex = 0;
+			// 
+			// tabPage6
+			// 
+			this.tabPage6.Controls.Add(this.textBox1);
+			this.tabPage6.Controls.Add(this.label1);
+			this.tabPage6.Location = new System.Drawing.Point(4, 22);
+			this.tabPage6.Name = "tabPage6";
+			this.tabPage6.Size = new System.Drawing.Size(556, 253);
+			this.tabPage6.TabIndex = 5;
+			this.tabPage6.Text = "Parameter Modes";
+			this.tabPage6.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(43, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(114, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(25, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "k";
+			// 
 			// tabPage2
 			// 
 			this.tabPage2.Controls.Add(this.textBox2);
@@ -91,6 +128,30 @@ namespace Obsidian
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "b";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(6, 224);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(492, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(504, 222);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(46, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(6, 6);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(544, 212);
+			this.listBox1.TabIndex = 0;
 			// 
 			// tabPage3
 			// 
@@ -119,64 +180,6 @@ namespace Obsidian
 			this.tabPage5.Text = "g";
 			this.tabPage5.UseVisualStyleBackColor = true;
 			// 
-			// tabPage6
-			// 
-			this.tabPage6.Controls.Add(this.textBox1);
-			this.tabPage6.Controls.Add(this.label1);
-			this.tabPage6.Location = new System.Drawing.Point(4, 22);
-			this.tabPage6.Name = "tabPage6";
-			this.tabPage6.Size = new System.Drawing.Size(556, 253);
-			this.tabPage6.TabIndex = 5;
-			this.tabPage6.Text = "Parameter Modes";
-			this.tabPage6.UseVisualStyleBackColor = true;
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(25, 15);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "k";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(43, 6);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(114, 21);
-			this.textBox1.TabIndex = 1;
-			// 
-			// listBox1
-			// 
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(6, 6);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(544, 212);
-			this.listBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(504, 222);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(46, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Add";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(6, 224);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(492, 21);
-			this.textBox2.TabIndex = 2;
-			// 
-			// chkSingleModes
-			// 
-			this.chkSingleModes.FormattingEnabled = true;
-			this.chkSingleModes.Location = new System.Drawing.Point(6, 6);
-			this.chkSingleModes.Name = "chkSingleModes";
-			this.chkSingleModes.Size = new System.Drawing.Size(544, 244);
-			this.chkSingleModes.TabIndex = 0;
-			// 
 			// mcModeList
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -187,10 +190,10 @@ namespace Obsidian
 			this.Text = "mcModeList";
 			this.tabControl1.ResumeLayout(false);
 			this.tabPage1.ResumeLayout(false);
-			this.tabPage2.ResumeLayout(false);
-			this.tabPage2.PerformLayout();
 			this.tabPage6.ResumeLayout(false);
 			this.tabPage6.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
 			this.ResumeLayout(false);
 		}
 		private System.Windows.Forms.CheckedListBox chkSingleModes;
